8f668c61d5322395bc8ddf81c068f29fe921d60f
Sandeep Yadav <sandeep.yadav@siemens.com>
2018-06-28 14:55:09 +0530

    get data for a asset

--- anomaly-detection/src/app/services/mindsdk.service.ts
+++ anomaly-detection/src/app/services/mindsdk.service.ts
@@ -13,7 +13,7 @@ export class MindsdkService {
     console.log('loading assets..');
     console.log('baseUrl: ' + environment.baseUrl);
      return this.http.get(
-      environment.baseUrl + 'api/assetmanagement/v3/assets?page=0&size=1000')
-      .pipe(map((response: any) => response.json()));
+      environment.baseUrl + '/api/iottimeseries/v3/timeseries/3cc1e87c3d794e3caf5d018f06646fbd/power?from=2018-05-01T06:15:47.745Z&to=2018-06-21T06:15:47.745Z')
+      .pipe(map((response: any) => console.log(response)));
   }
 }
